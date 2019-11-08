798856695e93ba54ff0cf36f3f4a1baec61ff88f
Bannon Tanner <bannon.nicholas.tanner@ibm.com>
2019-11-08 14:13:52 -0600

    3.2

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const numPersons = persons.length
+  const date = new Date()
+
+  res.send(`<p>Phonebook has info for ${numPersons} people</p> <p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
